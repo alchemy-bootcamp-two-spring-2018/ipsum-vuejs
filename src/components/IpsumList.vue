3e53610f856d1d29f1738fcaf46b39cd99a3c40c
rbtprograms <rbt61690@gmail.com>
2018-05-30 13:21:37 -0700

    things are on the screen!

---
+++ src/components/IpsumList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>List of Ipsums ({{list.length}})</h1>
+    <ul>
+      <li
+      v-for="ipsum in list"
+      >{{ ipsum.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ipsum from '../Ipsums.js'
+
+export default {
+  data() {
+    return {
+      list: ipsum
+    }
+  },
+
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
