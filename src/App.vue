901ef0020ca06b023a256922a3abb2d541081e29
carriecarter <carrie@carrie-carter.com>
2018-05-30 15:47:47 -0700

    tech issues and shut downs. yikes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>Lorem Ipsum Viewer</h1>
+    </header>
+    <main>
+      <section class="list">
+        <IpsumList 
+          v-bind:selected="selected"
+          v-on:select="updateSelected"
+        />
+      </section>
+      <section class="viewer">
+        <IpsumViewer v-bind:ipsum="selected"/>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import IpsumList from './components/IpsumList.vue';
+import IpsumViewer from './components/IpsumViewer.vue';
 
 export default {
-  name: 'app',
+  data() {
+    return {
+      selected: null,
+      someHtml: '<strong>hello</strong>'
+    }
+  },
   components: {
-    HelloWorld
+    IpsumList,
+    IpsumViewer
+  },
+  methods: {
+    updateSelected(ipsum) {
+      this.selected = ipsum;
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  -moz-osx-font-smoothing: grayscale; 
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.list {
+  float: left;
+  max-width: 500px;
+  background: rgb(101, 79, 110);
+}
+
+.viewer {
+  display: inline-block;
+}
+
 </style>
---
+++ src/components/IpsumList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Select An Ipsum To Preview:</h1>
+    <ul>
+      <li 
+          v-for="title in list"
+          v-bind:key="title"
+          v-on:click="$emit('select', title)"
+      >
+        <article v-bind:class="['title', { selected: title === selected }]">
+        </article>
+      </li>
+    </ul>
+  </div>
+</template>
