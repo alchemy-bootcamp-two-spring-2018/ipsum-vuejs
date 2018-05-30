4b302f30e4c452aa61e2d4e842d50c31e230f04b
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-05-30 13:40:34 -0700

    Initial commit plus list template

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Ipsum Viewer</h1>
+    </header>
+    <main>
+      <section class="list">
+        <IpsumList></IpsumList>
+      </section>
+      <section class="viewer">
+        <IpsumViewer></IpsumViewer>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+import IpsumList from './components/IpsumList.vue'
+import IpsumViewer from './components/IpsumViewer.vue'
+
+export default {
+  name: 'app',
+  components: {
+    IpsumList,
+    IpsumViewer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/IpsumList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{list.length}} Ipsums</h1>
+    <ul>
+      <li
+        v-for="ipsums in list"
+        v-bind:key="ipsums.author"
+        v-on:click="$emit('select', ipsums)"
+      >
+      <article>{{ipsums.title}}</article>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ipsums from '../data.js';
+
+export default {
+  data() {
+    return {
+      list: ipsums
+    }
+  },
+
+};
+</script>
+
+<style>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+</style>
