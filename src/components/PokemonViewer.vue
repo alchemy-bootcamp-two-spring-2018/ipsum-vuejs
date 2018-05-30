796d16fc25467365b2d719e6d70d5f3d7bb943e8
Ryan Luras <ryanluras@gmail.com>
2018-05-30 15:35:23 -0700

    everything is good to go, need to style now

--- src/components/PokemonViewer.vue
+++ src/components/PokemonViewer.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
         <article v-if="pokemon">
-            <h3>{{pokemon.pokemon}}</h3>
+            <img v-bind:src="pokemon.url_image">
+            <h2>{{pokemon.pokemon.toUpperCase()}}</h2>
+            <p>Types: {{pokemon.type_1.toUpperCase()}}, {{pokemon.type_2.toUpperCase()}}</p>
+            <p>HP: {{pokemon.hp}}</p>
+            <p>Attack: {{pokemon.attack}}</p>
+            <p>Defense: {{pokemon.defense}}</p>
         </article>
         <p v-else>Welcome Trainer! Please Choose a Pokémon.</p>
     </div>
