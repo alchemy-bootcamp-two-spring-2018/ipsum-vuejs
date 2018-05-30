cb5c2dd7c358af153cec34ca62b2c6dd659970e9
Chris Golden <chrgolden@gmail.com>
2018-05-30 13:44:33 -0700

    basic scaffolding done

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div id="app">
+    <header>
+        <h1> Ipsum Factum Viewer</h1>
+    </header>
+    <main>
+        <section class="list">
+            list section
+        </section>
+        <section class="viewer">
+            viewer section
+        </section>
+    </main>
+
+</div>
+</template>
+
+<script>
+// export default {
+//     data() {
+//         return {
+//             selected
+//         }
+//     }
+// }
+</script>
+
+<style>
+
+</style>
