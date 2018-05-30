86a467dcddd38767564906570308f1802b09ae80
MariahAdams <mariah.adams23@yahoo.com>
2018-05-30 14:39:13 -0700

    added scaffolding for IpsumList file

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
       <section class="ipsum-list">
         <IpsumList
           v-bind:selected="selected"
-
+          v-on:select="updateSelected"
         />
       </section>
       <section class="ipsum-viewer">
@@ -47,23 +47,3 @@ export default {
 }
 </style>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-
-<!--
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style> 
--->
--- src/components/IpsumList.vue
+++ src/components/IpsumList.vue
@@ -1,16 +1,42 @@
 <template>
-  <div id="app">
+  <div>
+    <h1>List of Ipsums ({{list.length}})</h1>
+    <ul>
+      <li
+        v-for="ipsum in list"
+        v-bind:key="ipsum.title"
+        v-on:click="$emit('select', ipsum)"
+      >
+        <section v-bind:class="['ipsum', {selected: pokemon === selected}]">
+          <span>{{ipsum.title}}</span>
+        </section>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ipsum from '../ipsums.js'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      list: ispum
+    }
+  },
+  props: ['selected']
 }
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 0 10px;
+}
+
+</style> 
