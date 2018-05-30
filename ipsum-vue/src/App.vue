502018212335c406277fbfcb05338f0b4d363f8a
Robyn Navarro <rnavarro182@gmail.com>
2018-05-30 15:39:32 -0700

    IpsumList renders

---
+++ ipsum-vue/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Ipsum finder</h1>
+      <p v-html="selectMessage"></p>
+    </header>
+    <main>
+      <section class="list">
+        <ipsum-list
+        v-bind:selected="selected"
+        v-on:select="updateSelected"
+        />
+      </section>
+
+    </main>
+  </div>
+</template>
+
+<script>
+import IpsumList from './components/IpsumList.vue';
+
+export default {
+  data() {
+    return {
+      selected: null,
+      selectMessage: '<span>Select an Ipsum</span>'
+    }
+  },
+  components: {
+    IpsumList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ipsum-vue/src/components/IpsumList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>List of Ipsums: {{list.length}}.</h1>
+        <ul>
+            <li
+            v-for="ipsum in list"
+            v-bind:key="ipsum.title"
+            v-on:click="$emit('select', ipsum)"
+            >
+                <article>
+                    <span>{{ipsum.title}}</span>
+
+                </article>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import ipsum from '../data.js';
+
+export default {
+    data() {
+        return {
+            list: ipsum
+        }
+    },
+    // props: ['selected']
+};
+</script>
+
