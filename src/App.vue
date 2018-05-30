94f1fd361fc0c08f4efb835bd95b7a9a79d39c23
Ryan Luras <ryanluras@gmail.com>
2018-05-30 14:40:36 -0700

    just figured out how to commit, i thought terminal was taken over by vue

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <header>
+    <h1>Vué.js Pokémon Pokédex</h1>
+    </header>
+    <main>
+      <section class="list">
+        <Pokemon-list/>
+      </section>
+      <section class="viewer">
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+import PokemonList from './components/PokemonList.vue';
+
+export default {
+  data() {
+    return {
+      selected: null
+    }
+
+  },
+  components: {
+    PokemonList,
+    
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 50px;
+}
+
+
+.list {
+  float: left;
+  /* max-width: 30%; */
+}
+</style>
---
+++ src/components/PokemonList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Pokémon Captured: {{list.length}} </h1>
+        <ul>
+            <li
+            v-for="pokemon in list"
+            v-bind:key="pokemon.id"
+            
+            >
+            <span>
+            {{pokemon.pokemon}}
+          </span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import pokemon from '../pokemon.js';
+
+export default {
+    data() {
+        return {
+            list: pokemon
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
